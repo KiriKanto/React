6578faba63d801899edb9adcd134ec03824b4baf
Martin Søborg-Busck <110230@RTS-365.dk>
2016-09-30 12:09:26 +0200

    initial commit

---
+++ my-react-app/webpack.config.js
@@ -0,0 +1,20 @@
+var path    = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry:[
+        './src/app'
+    ],
+    devtool: 'eval-source-map',
+    output: {
+        path: __dirname,
+        filename: 'app.js',
+        publicPath: '/js/'
+    },
+    module: {
+        loaders: [{
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
